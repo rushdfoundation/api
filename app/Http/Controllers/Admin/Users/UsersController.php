78218f92e0aebf9dde3026fbfae239ae0dfba209
ziagit <zia.csco@gmail.com>
2024-03-06 14:41:46 +0430

    order

--- app/Http/Controllers/Admin/Users/UsersController.php
+++ app/Http/Controllers/Admin/Users/UsersController.php
@@ -20,7 +20,7 @@ class UsersController extends Controller
      */
     public function index()
     {
-        $users = User::with('roles.permissions')->paginate(30);
+        $users = User::with('roles.permissions')->get();
         return response()->json($users);
     }
 
